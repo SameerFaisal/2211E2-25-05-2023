635277a7c0a89b78251c2fdc3b0e3d78aeb36a05
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-25 17:50:30 +0500

    2211E2 25-05-2023

---
+++ app.js
@@ -0,0 +1,62 @@
+// alert("Hello World")
+
+// let snake_case
+
+// let camelCase
+
+
+// let alphabet=prompt("Enter an alphabet")
+// switch(alphabet.toLowerCase()){
+//     case "a":
+//         alert("Alphabet A is Vowel")
+//         break;
+//     case "e":
+//         alert("Alphabet E is Vowel")
+//         break;
+//     case "i":
+//         alert("Alphabet I is Vowel")
+//         break;
+//     case "o":
+//         alert("Alphabet O is Vowel")
+//         break;
+//     case "u":
+//         alert("Alphabet U is Vowel")
+//         break;
+//     default:
+//         alert("Alphabet is not Vowel")
+// }
+
+// let user=+prompt("Enter a number")
+// if(user>0){
+//     alert("Positive")
+// }
+// else if(user<0){
+//     alert("Negative")
+// }
+
+// else{
+//     alert("Zero")
+// }
+// let urdu=+prompt("Enter urdu marks out of 100")
+// let maths=+prompt("Enter maths marks out of 100")
+// let english=+prompt("Enter english marks out of 100")
+// let obtainedMarks=urdu+english+maths
+// let totalMarks=300
+// let percentage=(obtainedMarks/totalMarks)*100
+// alert("Your percentage is "+percentage)
+// if(percentage>=80){
+//     alert("A-One")
+// }
+// else if(percentage>=70&&percentage<80){
+// alert("A")
+// }
+// else if(percentage>=60&&percentage<70){
+//     alert("B")
+// }
+// else if(percentage<60){
+//     alert("Fail")
+// }
+// else{
+//     alert("Invalid Input")
+
+// }
